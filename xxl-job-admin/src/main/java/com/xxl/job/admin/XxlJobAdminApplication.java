a3eb07256b9a11ebe5836cdc30a3826f6fc897eb
“cuiguoqiang <cuiguoq@bsoft.com.cn>
2019-09-02 23:04:34 +0800

    1. 文档 2. 统一认证

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -2,10 +2,12 @@ package com.xxl.job.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author xuxueli 2018-10-28 00:38:13
  */
+//@ImportResource("classpath*:config/spring.xml")
 @SpringBootApplication
 public class XxlJobAdminApplication {
 
